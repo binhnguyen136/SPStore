bf3e394b626f73391b4cb2852e8c242c665882b2
namcool <phuongnam091097@gmail.com>
2017-12-14 15:45:55 +0700

    update db

--- db_phoneshop.sql
+++ db_phoneshop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th12 13, 2017 lúc 08:44 SA
+-- Thời gian đã tạo: Th12 14, 2017 lúc 08:44 SA
 -- Phiên bản máy phục vụ: 5.7.14
 -- Phiên bản PHP: 7.0.10
 
@@ -35,6 +35,18 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `admin_cate_parent_list` ()  BEGIN
                     ON cate.parent_id = parent_cate.id
                     WHERE cate.id = cate.parent_id
                     ORDER BY cate.ordinal;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `cart_find_id` (IN `_id` INT(11))  BEGIN
+	SELECT * FROM cart WHERE cart.customer_id = _id;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `cart_insert` (IN `_id` INT(11), IN `_total` INT(11))  BEGIN
+	INSERT INTO cart(customer_id,total,created_at,updated_at) VALUES (_id,_total,NOW(),NOW());
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `cart_item_insert` (IN `_cart_id` INT(11), IN `_product_id` INT(11), IN `_quantity` INT(11))  BEGIN
+	INSERT INTO cart_item(cart_id,product_id,quantity,created_at,updated_at) VALUES (_cart_id,_product_id,_quantity,NOW(),NOW());
 END$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `category_id` (IN `id` INT(11))  BEGIN
@@ -175,6 +187,13 @@ CREATE TABLE `cart` (
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Đang đổ dữ liệu cho bảng `cart`
+--
+
+INSERT INTO `cart` (`id`, `customer_id`, `total`, `created_at`, `updated_at`) VALUES
+(16, 1, 32000000, '2017-12-14 08:42:22', '2017-12-14 08:42:22');
+
 -- --------------------------------------------------------
 
 --
@@ -189,6 +208,14 @@ CREATE TABLE `cart_item` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `cart_item`
+--
+
+INSERT INTO `cart_item` (`id`, `cart_id`, `product_id`, `quantity`, `created_at`, `updated_at`) VALUES
+(3, 16, 6, 3, '2017-12-14 08:42:22', '2017-12-14 08:42:22'),
+(4, 16, 19, 4, '2017-12-14 08:42:22', '2017-12-14 08:42:22');
 
 -- --------------------------------------------------------
 
@@ -210,7 +237,7 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`id`, `parent_id`, `name`, `ordinal`, `created_at`, `updated_at`) VALUES
-(1, 1, 'Mobile', 1, '2017-12-13 08:41:26', '2017-12-13 01:41:26'),
+(1, 1, 'Mobile', 1, '2017-12-13 08:54:52', '2017-12-13 01:54:52'),
 (2, 2, 'Tablet', 2, '2017-12-04 07:58:53', '2017-12-04 00:58:53'),
 (3, 3, 'Laptop', 3, '2017-12-04 07:58:46', '2017-12-04 00:58:46'),
 (4, 4, 'Accessory', 4, '2017-12-04 08:24:26', '2017-12-04 01:24:26'),
@@ -314,7 +341,7 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `image`, `image1`, `image2`, `image3`, `cate_id`, `detail`, `primary_cost`, `cost`, `quantity`, `created_at`, `updated_at`) VALUES
-(1, 'Galaxy J7 plus', 'samsung-galaxy-j7-plus-1-400x460.png', 'samsung-galaxy-j7-plus-1-400x460.png', 'samsung-galaxy-j7-plus-1-400x460.png', 'samsung-galaxy-j7-plus-1-400x460.png', 6, 'this is samsung smart phone', 5000000, 4000000, 0, '2017-12-12 07:57:52', '2017-12-04 07:27:11'),
+(1, 'Galaxy J7 plus', 'samsung-galaxy-j7-plus-1-400x460.png', 'samsung-galaxy-j7-plus-1-400x460.png', 'samsung-galaxy-j7-plus-1-400x460.png', 'samsung-galaxy-j7-plus-1-400x460.png', 6, 'this is samsung smart phone', 5000000, 4000000, 15, '2017-12-13 09:46:59', '2017-12-13 02:46:59'),
 (2, 'Galaxy Note FE', 'samsung-galaxy-note-fe-ha-400x460.png', 'samsung-galaxy-note-fe-ha-400x460.png', 'samsung-galaxy-note-fe-ha-400x460.png', 'samsung-galaxy-note-fe-ha-400x460.png', 6, 'this is samsung smart phone', 5000000, 5000000, 0, '2017-12-04 07:33:12', '2017-12-04 07:33:12'),
 (3, 'Galaxy S8 plus', 'samsung-galaxy-s8-plus-tim-khoi-400-400x460.png', 'samsung-galaxy-s8-plus-tim-khoi-400-400x460.png', 'samsung-galaxy-s8-plus-tim-khoi-400-400x460.png', 'samsung-galaxy-s8-plus-tim-khoi-400-400x460.png', 6, 'this is samsung smart phone', 5000000, 5000000, 0, '2017-12-04 09:04:30', '2017-12-04 09:04:30'),
 (4, 'Galaxy S8', 'samsung-galaxy-s8-4-400x460-400x460.png', 'samsung-galaxy-s8-4-400x460-400x460.png', 'samsung-galaxy-s8-4-400x460-400x460.png', 'samsung-galaxy-s8-4-400x460-400x460.png', 6, 'this is samsung smart phone', 5000000, 5000000, 0, '2017-12-04 09:07:44', '2017-12-04 09:07:44'),
@@ -356,7 +383,7 @@ CREATE TABLE `slides` (
 --
 
 INSERT INTO `slides` (`id`, `image`, `title`, `content`, `link`, `ordinal`, `created_at`, `updated_at`) VALUES
-(12, '01_12_2017_10_25_40_Samsung-Big-Note-8-800-300-GIF-1.gif', 'xxx', 'xx', 'google.com', 1, '2017-12-13 01:02:28', '2017-12-13 01:02:28'),
+(12, '01_12_2017_10_25_40_Samsung-Big-Note-8-800-300-GIF-1.gif', 'xxx', 'xxzzzzz', 'google.com', 1, '2017-12-13 08:55:25', '2017-12-13 01:55:25'),
 (13, '28_11_2017_11_07_28_iphoneX-800-300.png', 'xwx', 'wxw', 'google.com', 2, '2017-12-13 01:02:39', '2017-12-13 01:02:39');
 
 -- --------------------------------------------------------
@@ -382,7 +409,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `phone`, `address`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'nam', 'nam@gmail.com', '$2y$10$Yh5GWoyIWl1mSoxnP10PjekHXln0sYr4WBw8pMGaEnxSia0LrFTXS', NULL, NULL, 'GXLP0n6C3VHE4vufDYR6oJKftZp43NL9iNlDNV4aFVavtd2xJ9A6edjeY6cu', '2017-12-12 03:49:40', '2017-12-12 03:50:36');
+(1, 'nam', 'nam@gmail.com', '$2y$10$Yh5GWoyIWl1mSoxnP10PjekHXln0sYr4WBw8pMGaEnxSia0LrFTXS', '0917218297', 'HoChiMinh City', '73Mcb60UkOfa3Mn6Tf0YhUETv7XP0u1Ac527ixbZwYGY6eYmYfwOxxwI6IcQ', '2017-12-12 03:49:40', '2017-12-14 01:06:38'),
+(2, 'namcool', 'nam10@gmail.com', '$2y$10$n4XBZapz.jHkSxwamcYrW.MX.90c4DO2VufAre04RxYxk/zXAgsVu', NULL, NULL, 'gFD17rATNyltaupMWCerPZtmjcVU1jZKXvKKM5twiH02CUmHfiuK4Xed8DNj', '2017-12-13 02:26:04', '2017-12-13 02:28:09');
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -469,17 +497,17 @@ ALTER TABLE `admins`
 -- AUTO_INCREMENT cho bảng `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 --
 -- AUTO_INCREMENT cho bảng `cart_item`
 --
 ALTER TABLE `cart_item`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT cho bảng `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT cho bảng `orders`
 --
@@ -509,7 +537,7 @@ ALTER TABLE `slides`
 -- AUTO_INCREMENT cho bảng `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- Các ràng buộc cho các bảng đã đổ
 --
